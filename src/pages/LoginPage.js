47da91dbe7fb8b7f2caa89aeda501467e70a6c75
osll1 <129036799+osll1@users.noreply.github.com>
2024-09-20 17:11:00 +0300

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -25,7 +25,7 @@ const LoginPage = () => {
   const navigate = useNavigate();
 
 
-  const API_URL = process.env.API_URL;
+  const API_URL = process.env.REACT_APP_API_URL;
 
   const handleLogin = async () => {
     try {
