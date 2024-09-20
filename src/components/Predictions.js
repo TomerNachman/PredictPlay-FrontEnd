b0b0588583eb08759a6cc8be69fb57ade082afec
osll1 <129036799+osll1@users.noreply.github.com>
2024-09-20 16:45:39 +0300

    Update Predictions.js

--- src/components/Predictions.js
+++ src/components/Predictions.js
@@ -22,7 +22,7 @@ const Predictions = () => {
   const [games, setGames] = useState([]);
 
 
-  const API_URL = process.env.API_URL;
+  const API_URL = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
     // הבאת כל המשחקים מהשרת
